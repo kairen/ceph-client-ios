0ec9e3edd0ff7b387319159e2059f1cfb67a27e5
kairen.bai <s1499k005@gmail.com>
2015-06-03 15:31:24 +0800

    Initial Commit

---
+++ CephMonitor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CephMonitor
+//
+//  Created by KaiRen.Bai on 2015/6/3.
+//  Copyright (c) 2015年 KaiRen.Bai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
